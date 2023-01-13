da1f8c20777f2333f44a50fa46b5f722524c0cbb
John <Johnpioc15@gmail.com>
2023-01-13 14:20:41 +1000

    fixed server bugs

--- server/controllers/openaiController.js
+++ server/controllers/openaiController.js
@@ -1,13 +1,17 @@
 import { Configuration, OpenAIApi } from "openai";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const configuration = new Configuration({ apiKey: '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' });
+const apiKey = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({ apiKey });
 
 const openai = new OpenAIApi(configuration);
 
 export const generateImage = async (req, res) => {
     const { prompt, size } = req.body;
 
-    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024'
+    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
     try {
         const response = await openai.createImage({
             prompt,
