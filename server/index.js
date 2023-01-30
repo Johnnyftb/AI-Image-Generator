04fba3253f56de1bbda1787675fe129d2e6811d2
John <Johnpioc15@gmail.com>
2023-01-30 21:44:26 +1000

    final changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import routes from './routes/openaiRoutes.js';
 dotenv.config();
 
@@ -7,6 +8,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
